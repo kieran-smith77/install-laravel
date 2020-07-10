66438107ab243ed1c061a6884ee937816f904e13
jameskieransmith <kieran_smith77@hotmail.com>
2020-07-10 10:40:09 +0100

    Create ubuntu_install.sh

---
+++ ubuntu_install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+######
+# Install LAMP Stack
+######
+
+apt update -y
+apt install -y apache2 mysql-server php php-mysql libapache2-mod-php php-cli
+apt install -y git php-bcmath php-ctype php-fileinfo php-json php-mbstring php-openssl php-pdo php-tokenizer php-xml
+
+systemctl enable apache2
+systemctl start apache2
+
+######
+# Install Composer
+######
+
+EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+mv composer.phar /usr/bin/composer
+
+######
+# Install Laravel
+######
+
+composer global require laravel/installer
+
+export PATH="~/.config/composer/vendor/bin:$PATH" 
+
+laravel new test
